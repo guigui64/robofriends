0add19857d38743c428618a9a7c1d38f1abc06e9
guigui64 <guillaume.comte10@gmail.com>
2019-02-10 21:02:47 +0100

    Updates

---
+++ precache-manifest.59af8cf0e8c614fd89ee563bc585f031.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "12a3db8ba7f5423327bb12aed3c41977",
+    "url": "/robofriends/static/media/SEGA.12a3db8b.woff"
+  },
+  {
+    "revision": "1446d6ec194445c3572d",
+    "url": "/robofriends/static/js/runtime~main.1446d6ec.js"
+  },
+  {
+    "revision": "8a1383d6143d862150c3",
+    "url": "/robofriends/static/js/main.8a1383d6.chunk.js"
+  },
+  {
+    "revision": "39f45f5dc1d53045079f",
+    "url": "/robofriends/static/js/1.39f45f5d.chunk.js"
+  },
+  {
+    "revision": "8a1383d6143d862150c3",
+    "url": "/robofriends/static/css/main.9273cacd.chunk.css"
+  },
+  {
+    "revision": "39f45f5dc1d53045079f",
+    "url": "/robofriends/static/css/1.3f7d0576.chunk.css"
+  },
+  {
+    "revision": "bed28708d6cea02ad202742e77120378",
+    "url": "/robofriends/index.html"
+  }
+];
